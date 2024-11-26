f2891938eb547bad8e42e613fa653a9efb801880
totegamma <github@gammalab.net>
2024-11-26 18:57:54 +0900

    remove gen command exec

--- cmd/generate.go
+++ cmd/generate.go
@@ -1,17 +1,12 @@
 package cmd
 
 import (
-	"fmt"
-
 	"github.com/spf13/cobra"
 )
 
 var generateCmd = &cobra.Command{
 	Use:     "generate",
 	Aliases: []string{"gen"},
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("generate called")
-	},
 }
 
 func init() {
